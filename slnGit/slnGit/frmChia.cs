fa57fce8b38cef2bf0cd40330203c4f8e41b9d4f
TrungNguyen <viettrung10035@gmail.com>
2018-08-08 13:31:05 +0700

    demo

---
+++ slnGit/slnGit/frmChia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slnGit
+{
+    public partial class frmChia : Form
+    {
+        public frmChia()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double kq = a / b;
+            if(b == 0)
+            {
+                MessageBox.Show("Khong chia duoc ");
+
+            }
+            textBox3.Text = kq.ToString();
+        }
+    }
+}
---
+++ slnGit/slnGit/frmMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slnGit
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmCong fc = new frmCong();
+            fc.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmTru ft = new frmTru();
+            ft.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmNhan fn = new frmNhan();
+            fn.Show();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            frmChia fc = new frmChia();
+            fc.Show();
+        }
+    }
+}
